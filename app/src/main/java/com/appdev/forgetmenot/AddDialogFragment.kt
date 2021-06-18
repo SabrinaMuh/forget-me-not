32a868c3de51c02c9e7f96f6dc830f76897b5b14
SabrinaMuh <sabrina.muhrer@aon.at>
2021-06-18 15:31:40 +0200

    designed dialog fragment

--- app/src/main/java/com/appdev/forgetmenot/AddDialogFragment.kt
+++ app/src/main/java/com/appdev/forgetmenot/AddDialogFragment.kt
@@ -4,6 +4,7 @@ import android.app.AlertDialog
 import android.app.Dialog
 import android.content.DialogInterface
 import android.os.Bundle
+import android.widget.TimePicker
 import androidx.fragment.app.DialogFragment
 import java.lang.IllegalStateException
 
@@ -13,9 +14,13 @@ class AddDialogFragment : DialogFragment() {
         return activity?.let {
             val builder = AlertDialog.Builder(it)
             val inflater = requireActivity().layoutInflater
-
-            builder.setView(inflater.inflate(R.layout.dialog_add, null))
-                .setPositiveButton(R.string.add,
+            val layout = inflater.inflate(R.layout.dialog_add, null)
+            val timePickerStart = layout.findViewById<TimePicker>(R.id.time_picker_start)
+            timePickerStart.setIs24HourView(true)
+            val timePickerEnd = layout.findViewById<TimePicker>(R.id.time_picker_end)
+            timePickerEnd.setIs24HourView(true)
+            builder.setView(layout)
+                .setPositiveButton(R.string.save,
                     DialogInterface.OnClickListener { dialog, id ->
 
                     })
@@ -26,4 +31,5 @@ class AddDialogFragment : DialogFragment() {
             builder.create()
         } ?:throw IllegalStateException("Activity cannot be null")
     }
+
 }
