34db353e4291f9d9bc013e653ed32e4702d80c0e
SabrinaMuh <sabrina.muhrer@aon.at>
2021-06-18 18:56:13 +0200

    add methode to pass datas to MainActivity

--- app/src/main/java/com/appdev/forgetmenot/MainActivity.kt
+++ app/src/main/java/com/appdev/forgetmenot/MainActivity.kt
@@ -7,9 +7,13 @@ import android.widget.ArrayAdapter
 import android.widget.ListView
 import android.widget.Toast
 import androidx.annotation.RequiresApi
+import androidx.fragment.app.DialogFragment
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import java.sql.Timestamp
+import java.text.SimpleDateFormat
 import java.time.LocalDateTime
+import java.util.*
+import kotlin.collections.ArrayList
 
 /*
  * Forget me not
@@ -17,13 +21,13 @@ import java.time.LocalDateTime
  * @authers: Sabrina Muhrer, Harald Moitzi
  */
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AddDialogFragment.NoticeDialogListener{
+    var alMainEntries: ArrayList<MainEntry> = ArrayList<MainEntry>()
+
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        var alMainEntries: ArrayList<MainEntry> = ArrayList<MainEntry>()
 
         var dateTime = LocalDateTime.of(2021, 6, 15, 8, 0, 0, 0)
 
@@ -64,4 +68,13 @@ class MainActivity : AppCompatActivity() {
             dialog.show(supportFragmentManager, "add")
         }
     }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onDialogPositiveClick(title: String, category: String, startDay: Int,
+                                       startMonth: Int, startYear: Int, startTimeHour: Int, startTimeMinute: Int, frequency: String,
+                                       endDay: Int, endMonth: Int, endYear: Int, endTimeHour: Int, endTimeMinute: Int
+    ) {
+        alMainEntries.add(MainEntry(title, category, LocalDateTime.of(startYear, startMonth, startDay, startTimeHour, startTimeMinute)))
+    }
+
 }
