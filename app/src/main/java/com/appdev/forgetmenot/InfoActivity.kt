2cf1030d89f9db1503b829033b21b83d7c04b8ab
SabrinaMuh <sabrina.muhrer@aon.at>
2021-07-21 20:45:17 +0200

    change info layout and add button to close info

--- app/src/main/java/com/appdev/forgetmenot/InfoActivity.kt
+++ app/src/main/java/com/appdev/forgetmenot/InfoActivity.kt
@@ -1,11 +1,18 @@
 package com.appdev.forgetmenot
 
+import android.app.Activity
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class InfoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar!!.hide()
         setContentView(R.layout.activity_info)
+        val closeButton: ImageView = findViewById(R.id.imageViewClose)
+        closeButton.setOnClickListener {
+            finish()
+        }
     }
 }
